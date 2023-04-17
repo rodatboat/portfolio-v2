209480fe9a1e78f496a0e4b1f08a0964c8fbf399
Ronaldo Suarez Surface <ronaldo3055@gmail.com>
2023-04-16 23:32:37 -0400

    fix

--- api/ssr.js
+++ api/ssr.js
@@ -1,21 +1,23 @@
 import { renderPage } from 'vite-plugin-ssr/server'
 
 export default async function handler(req, res) {
-  const { urlOriginal } = req
-  console.log('Request to url:', urlOriginal)
+  // const { urlOriginal } = req
+  // console.log('Request to url:', urlOriginal)
 
-  const pageContextInit = { urlOriginal }
-  const pageContext = await renderPage(pageContextInit)
-  const { httpResponse } = pageContext
+  // const pageContextInit = { urlOriginal }
+  // const pageContext = await renderPage(pageContextInit)
+  // const { httpResponse } = pageContext
 
-  if (!httpResponse) {
-    res.statusCode = 200
-    res.end()
-    return
-  }
+  // if (!httpResponse) {
+  //   res.statusCode = 200
+  //   res.end()
+  //   return
+  // }
 
-  const { body, statusCode, contentType } = httpResponse
-  res.statusCode = statusCode
-  res.setHeader('content-type', contentType)
-  res.end(body)
+  // const { body, statusCode, contentType } = httpResponse
+  // res.statusCode = statusCode
+  // res.setHeader('content-type', contentType)
+  // res.end(body)
+  const { httpResponse } = await renderPage({ urlOriginal: req.url })
+  res.send(httpResponse.body)
 }
