a529c47f90dbade3bc451e55962f9085826f2cf9
Ronaldo Suarez <ronaldo3055@gmail.com>
2023-04-24 22:02:09 -0400

    Added project layout 3, still need to add tech stacks

--- pages/index/Projects.jsx
+++ pages/index/Projects.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { RiGithubFill } from "react-icons/ri/index.js";
 
 const ProjectLayout1 = () => {
-    const [projects, setProjects] = useState([1]);
+    const [projects, setProjects] = useState([1, 1]);
 
     return (
         <>
@@ -20,8 +20,8 @@ const ProjectLayout1 = () => {
                             <a href="#" className='text-white hover:text-primary transition-all duration-300'>northstarmoney.com →</a>
                         </div>
                     </div>
-                    <div className='basis-1/3'>
-                        <a><img className='h-full w-full object-cover object-left-top p-1 rounded-t-lg' src="/lawnagency.png" /></a>
+                    <div className='basis-1/3 p-1'>
+                        <a href="#"><img className='h-full w-full object-cover object-left-top rounded-t-lg' src="/lawnagency.png" /></a>
                     </div>
                 </div>
             )
@@ -31,15 +31,15 @@ const ProjectLayout1 = () => {
 }
 
 const ProjectLayout2 = () => {
-    const [projects, setProjects] = useState([1, 1, 1, 1]);
+    const [projects, setProjects] = useState([1, 1]);
 
     return (
         <>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 sm:gap-6 pt-20'>
                 {projects.map((p, i) =>
                     <div key={i} className='flex flex-col'>
-                        <div className='basis-1/3'>
-                            <a><img className='h-full w-full object-cover object-left-top p-1 rounded-t-lg' src="/lawnagency.png" /></a>
+                        <div className='basis-1/3 p-1'>
+                            <a href="#"><img className='h-full w-full object-cover object-left-top border border-secondary rounded-lg' src="/lawnagency.png" /></a>
                         </div>
                         <div className='basis-2/3'>
                             <h2 className='text-2xl font-bold py-2'>Northstar</h2>
@@ -49,7 +49,7 @@ const ProjectLayout2 = () => {
                                 financial wellness.
                             </p>
                             <div className='flex flex-row items-center pt-3 text-xl gap-4 font-bold'>
-                                <a href="#" className='border border-secondary rounded-lg text-white hover:text-black bg-black hover:bg-white transition-all duration-300 p-1'>
+                                <a href="#" className='rounded-lg text-white hover:text-black bg-black hover:bg-white transition-all duration-300 p-1'>
                                     <RiGithubFill size={22} />
                                 </a>
                                 <a href="#" className='text-md text-white hover:text-primary transition-all duration-300'>northstarmoney.com →</a>
@@ -63,6 +63,37 @@ const ProjectLayout2 = () => {
     )
 }
 
+const ProjectLayout3 = () => {
+    const [projects, setProjects] = useState([1, 1]);
+
+    return (
+        <>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-6 pt-20'>
+            {projects.map((p, i) =>
+                <div key={i} className='flex flex-col md:flex-row gap-4 bg-primary rounded-xl pl-6 pt-6'>
+                    <div className='basis-2/3 pb-6'>
+                        <h2 className='text-4xl text-black font-bold py-10'>Northstar</h2>
+                        <p className='text-xl text-black'>My current full-time role, where I lead our design efforts as the Head of Design.
+                            I work on our product, am involved in strategic business decisions, and manage an
+                            incredible design team of talented folks as we build the future of
+                            financial wellness.
+                        </p>
+                        <div className='flex flex-row items-center pt-10 text-xl gap-4 font-bold'>
+                            <a href="#" className='rounded-lg text-black hover:text-primary bg-primary hover:bg-black transition-all duration-300 p-1'><RiGithubFill size={24} /></a>
+                            <a href="#" className='text-black transition-all duration-300 '>northstarmoney.com →</a>
+                        </div>
+                    </div>
+                    <div className='basis-1/3'>
+                        <a href="#"><img className='h-full w-full object-cover object-left-top rounded-t-lg' src="/lawnagency.png" /></a>
+                    </div>
+                </div>
+            )
+            }
+            </div>
+        </>
+    )
+}
+
 export default function Projects() {
     return (
         <>
@@ -70,7 +101,9 @@ export default function Projects() {
                 <h2 className='text-6xl font-bold'>Select Work</h2>
             </div>
             <div className=''>
+                <ProjectLayout1 />
                 <ProjectLayout2 />
+                <ProjectLayout3 />
             </div>
         </>
     )
