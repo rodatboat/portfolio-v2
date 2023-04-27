adf70aa89437532f706d88c6030f34883d7fcf0b
Ronaldo Suarez <ronaldo3055@gmail.com>
2023-04-26 21:49:44 -0400

    hiding feature posts since there is none atm

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -24,7 +24,7 @@ function Page(pageProps) {
       <About />
       <Projects />
       <Contributions />
-      <FeaturedPosts />
+      {/* <FeaturedPosts /> */}
       <Contact />
       <Footer />
     </>
